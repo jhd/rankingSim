b0837bba93edc256ea30b501f411398810704421
jhd <jonathanhennessydoyle@gmail.com>
2016-02-21 19:05:57 +0000

    Changed rank change to use Elo, added labels to plot

--- rankingSim.py
+++ rankingSim.py
@@ -21,17 +21,26 @@ def generatePlayers(numPlayers):
     players = []
     for i in range(0, numPlayers):
         skill = random.uniform(0, 10)
-        player = Player(i, skill, 0)
+        player = Player(i, skill, skill)
         players.append(player)
     return players
+
+def elo(playerA, playerB):
+    probA = 1/(1+pow(10, (playerB.rank - playerA.rank)/400))
+    probB = 1/(1+pow(10, (playerA.rank - playerB.rank)/400))
+
+    return (probA, probB)
 
 def nextRound(players):
     for playerA, playerB in list(itertools.combinations(players, 2)):
         variance = random.uniform(-1, 1)
+        (expectedA, expectedB) = elo(playerA, playerB)
         if playerA.skill > playerB.skill + variance:
-            playerA.rank += 1
+            playerA.rank += 1 - expectedA
+            playerB.rank -= 1 - expectedB
         elif playerB.skill + variance > playerA.skill:
-            playerB.rank += 1
+            playerA.rank -= 1 - expectedA
+            playerB.rank += 1 - expectedB
 
 def runSimulation(numPlayers, generations):
 
@@ -46,16 +55,17 @@ def runSimulation(numPlayers, generations):
     for player in players:
         rankHistory.append((generations, player.pid, player.rank))
     
-    for i in range(0, numPlayers):
+    for player in players:
         history = []
         for (rounds, pid, rank) in rankHistory:
-            if i == pid:
-                history.append((rounds, rank))
-        pyplot.plot(range(0, generations+1), history, 's-')
-            
+            if player.pid == pid:
+                history.append(rank)
+        pyplot.plot(range(0, generations+1), history, 's-', label = str(player.skill))
+           
+    pyplot.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=-5)
     pyplot.show()
 
     #for player in players:
     #    print player.pid, player.skill, player.rank
 
-runSimulation(10, 5)
+runSimulation(10, 50)
