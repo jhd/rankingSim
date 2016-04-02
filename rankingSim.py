a92a941922bb83b32a850fa764d822f9154f6d60
jhd <jonathanhennessydoyle@gmail.com>
2016-04-02 21:44:55 +0100

    Added ajacent palyer pairing support

--- rankingSim.py
+++ rankingSim.py
@@ -16,7 +16,7 @@ def pickPair(players, method, playerA = None):
         return itertools.combinations(players, 2)
     if method == 'similar':
         if playerA == None:
-            pass
+            return sorted(players, key = lambda x: x.rank)
         else:
             return min(players, key = lambda x: (abs(x.rank - playerA.rank)) if x.pid != playerA.pid else float('inf'))
     else: 
@@ -43,7 +43,9 @@ def elo(playerA, playerB):
     return (probA, probB)
 
 def nextRound(players):
-    for playerA, playerB in list(itertools.combinations(players, 2)):
+    #for playerA, playerB in list(itertools.combinations(players, 2)):
+    for playerA, playerB in zip(pickPair(players, 'similar'), pickPair(players, 'similar')[1:]):
+        print playerA.pid, playerA.rank, playerA.skill, playerB.pid, playerB.rank, playerB.skill
         variance = random.uniform(-1, 1)
         (expectedA, expectedB) = elo(playerA, playerB)
         if playerA.skill > playerB.skill + variance:
@@ -62,6 +64,9 @@ def runSimulation(numPlayers, generations):
     for i in range(0, generations):
         for player in players:
             rankHistory.append((i, player.pid, player.rank))
+
+        #for player in players:
+        #    print player.pid, player.rank, player.skill
         nextRound(players)
 
     for player in players:
@@ -75,7 +80,7 @@ def runSimulation(numPlayers, generations):
         pyplot.plot(range(0, generations+1), history, 's-', label = str(player.skill))
            
     pyplot.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=-1)
-    #pyplot.show()
+    pyplot.show()
 
     #for player in players:
     #    print player.pid, player.skill, player.rank
